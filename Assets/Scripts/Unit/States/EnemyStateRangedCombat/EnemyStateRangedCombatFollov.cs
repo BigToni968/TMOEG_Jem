fbadffd49299dab33bc38396b724c17eb548623c
Antos_Codes <temp88665@gmail.com>
2024-06-09 12:12:59 +0300

    Update prefabs Enemy
    In order for enemies to better switch between ranged and melee choices, each entity needs a projectile targeting.
    Attention, in the prefab settings you can see the gizmos rendering which will show and render the targeting area.
    For this prefab you need to specify Character in which Combat Mode should be selected. That has been updated to customize gizmos rendering.

---
+++ Assets/Scripts/Unit/States/EnemyStateRangedCombat/EnemyStateRangedCombatFollov.cs
@@ -0,0 +1,44 @@
+using MyStateMachine = Patterns.StateMachine;
+using UnityEngine;
+
+namespace Game
+{
+    public class EnemyStateRangedCombatFollov : EnemyStateRangedCombatBase
+    {
+        private LayerMask _bots;
+        public EnemyStateRangedCombatFollov(MyStateMachine machine) : base(machine) { }
+
+        public override void Start()
+        {
+            base.Start();
+            _bots = 1 << LayerMask.NameToLayer("Enemy");
+        }
+
+        public override void Update()
+        {
+            base.Update();
+
+            if (IsObstacle())
+            {
+                return;
+            }
+
+            Move();
+
+            if (Vector3.Distance(Control.Owner.transform.position, Control.Player.transform.position) <= CloseCombatMode.ReadData.Distance)
+            {
+                Control.Switch(new EnemyStateRangedCombatAttack(Control));
+            }
+        }
+
+        public virtual void Move()
+        {
+            Control.Owner.transform.Translate(Vector3.forward * Control.Owner.Stats.ReadData.Speed * Time.deltaTime);
+        }
+
+        public virtual bool IsObstacle()
+        {
+            return Physics.Raycast(Control.Owner.transform.position, Control.Owner.transform.forward, Control.Owner.Character.ObstacleDetectionDistance, _bots);
+        }
+    }
+}
